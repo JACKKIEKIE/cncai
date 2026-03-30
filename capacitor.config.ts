d9ef8c9e3dca056c148738da602c073e38b40362
JACKKIEKIE <jackoikpig@gmail.com>
2026-03-30 17:16:29 +0800

    chore: normalize app metadata

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.linguacnc.app',
-  appName: '灵语智造',
+  appName: 'LinguaCNC',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
